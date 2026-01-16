474fc964bb13ee9444900667bc3f2f14aede363d
TAREK <tareksayedmokhtar@yahoo.com>
2026-01-16 23:10:29 +0200

    Update admin.py

--- ip_tracking/admin.py
+++ ip_tracking/admin.py
@@ -1,1 +1,7 @@
+from django.contrib import admin
+from ip_tracking.models import RequestLog, BlockedIP, SuspiciousIP
+
+admin.site.register(RequestLog)
+admin.site.register(BlockedIP)
+admin.site.register(SuspiciousIP)
 
